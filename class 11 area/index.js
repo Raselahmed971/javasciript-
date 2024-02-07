d23ae92b2ed58afbce498fd24f7b1b95fd365c78
Rasel Ahmed <raselahmed.r2ait@gmail.com>
2024-02-07 19:24:03 +0600

    3ed comit

--- class 11 area/index.js
+++ class 11 area/index.js
@@ -1,14 +1,43 @@
-var num = parseFloat(prompt("enter you number: "));
+// var num = parseFloat(prompt("enter you number: "));
 
-if(num >= 80 && num <= 100) {
-    document.write("you got A+ ");
+// if(num >= 80 && num <= 100) {
+//     document.write("you got A+ ");
+// }
+// else if(num >=50 && num <=79 ){
+//     document.write("you got B ");
+// }
+// else if(num >= 30 && num <=49){
+//     document.write("you got c");
+// }
+// else{
+//     document.write("you fail");
+// }
+
+
+// for( var x = 1; x <= 10; x= x+1){
+//     document.write('hi' + "<br>");
+// }
+
+// for(let x=10; x>=1; x= x -2){
+//     document.write(x + "<br>");
+// }
+
+// document.write('end');
+
+// let sum = 0;
+// for( let x= 1; x <=5; x++){
+//     sum = sum + x;
+// }
+// document.write(sum);
+
+
+var firstNumber = parseInt(prompt("Enter the first number"));
+var secondNumber = parseInt(prompt("Enter the second number"));
+
+var sum = 0;
+
+for (var i = firstNumber; i <= secondNumber; i++) {
+  sum = sum + i;
 }
-else if(num >=50 && num <=79 ){
-    document.write("you got B ");
-}
-else if(num >= 30 && num <=49){
-    document.write("you got c");
-}
-else{
-    document.write("you fail");
-}
+
+document.write("The sum of numbers from " + firstNumber + " to " + secondNumber + " is: " + sum);
